42a9f3c2d67c3967a7e59fa719141eb4c34a53c0
natekonimbo <nate@konimbo.co.il>
2016-10-30 18:03:11 +0200

    init

---
+++ web-server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {
+        'content-type': 'text/plain'
+    });
+    res.write('hello from nodejs!');
+    res.end('good-bye!');
+});
+
+server.listen(8080);
